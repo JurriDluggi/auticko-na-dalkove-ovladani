61051041dd1085490d6dc9351d17358ed34ecc6c
JurriDluggi <jiri.dluhos@gmail.com>
2022-03-13 21:47:16 +0100

    pohyb radlice

--- main.ts
+++ main.ts
@@ -5,12 +5,49 @@ radio.onReceivedValue(function (name, value) {
     if (name == "y") {
         yValue = value
     }
+    if (name == "radlice") {
+        pohyb_radlice = value
+        if (pohyb_radlice == 1) {
+            basic.showLeds(`
+                . . # . .
+                . # # # .
+                . . # . .
+                . . # . .
+                . . . . .
+                `)
+        }
+        if (pohyb_radlice == -1) {
+            basic.showLeds(`
+                . . . . .
+                . . # . .
+                . . # . .
+                . # # # .
+                . . # . .
+                `)
+        }
+    }
 })
+let pravy = 0
+let levy = 0
 let yValue = 0
 let xValue = 0
+let pohyb_radlice = 0
 radio.setGroup(1)
-wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 90)
+let radlice = 10
+pohyb_radlice = 0
+wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, radlice)
 basic.forever(function () {
-    wuKong.setMotorSpeed(wuKong.MotorList.M1, yValue)
-    wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, xValue)
+    levy = yValue - xValue
+    pravy = yValue + xValue
+    wuKong.setMotorSpeed(wuKong.MotorList.M1, Math.constrain(levy, -50, 50))
+    wuKong.setMotorSpeed(wuKong.MotorList.M2, Math.constrain(pravy, -50, 50))
+    if (radlice >= 0) {
+        radlice = radlice + pohyb_radlice * 1
+    } else {
+        radlice = 0
+    }
+    if (radlice >= 40) {
+        radlice = 40
+    }
+    wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, radlice)
 })
